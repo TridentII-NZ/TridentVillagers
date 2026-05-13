0016a30e6b0420b93059ec55daf3a0458b6f6cb1
TridentII-NZ <dolladylan24@gmail.com>
2026-05-13 16:53:46 +1200

    Initial commit

---
+++ src/main/java/TridentII/TridentVillagers.java
@@ -0,0 +1,16 @@
+package TridentII;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TridentVillagers extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
